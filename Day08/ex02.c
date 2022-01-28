84011190b52cf9705c57d27c9a8ea33d12096cc1
Hyunni0 <hyunn09153@gmail.com>
2022-01-28 13:00:20 +0900

    8일차

---
+++ Day08/ex02.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/*
+    # 포인터와 배열의 관계
+
+*/
+int main(void){
+    int arr[3] = {1,2,3};
+
+    // 인덱스를 통한 배열 접근
+    printf("인덱스를 통한 배열 접근 \n");
+    printf("%d %d %d \n", arr[0], arr[1], arr[2]);
+
+    // 포인터 연산을 통한 배열 접근
+    
+
+}
---
+++ Day08/ex04.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+/*
+    배열 포인터
+    : 배열을 가리키는 포인터 변수
+*/
+int main(void){
+
+}
