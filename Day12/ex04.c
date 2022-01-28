fedb2f45c64fbd3ec88c2e1c4ae75d6d458803ed
Hyunni0 <hyunn09153@gmail.com>
2022-01-28 14:23:32 +0900

    12일차

--- Day12/ex04.c
+++ Day12/ex04.c
@@ -8,6 +8,8 @@ void printNumber(void){
     printf("전역 변수 number는 %d \n", number);
 }
 
+// 변수의 이름이 동일한 경우,
+// : 현재 { } 블록에서 선언된 지역변수가 우선된다.
 int main(void){
     // 지역 변수
     // : 선언된 중괄호 { } 블록 안에서만 접근 가능
